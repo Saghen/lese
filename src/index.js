fd02374c2f382787bdbc485666a49ff11e58c6e8
Liam Dyer <saghendev@gmail.com>
2019-09-13 18:43:12 -0400

    Update flex to container

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Base from './components/Base';
-import Container from './components/Flex';
+import Container from './components/Container';
 import helpers from './helpers';
 
 export { Base, Container, helpers }
