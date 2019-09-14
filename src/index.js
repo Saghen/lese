9100773367468a23cd2f7803554b6850dffd36ea
Liam Dyer <saghendev@gmail.com>
2019-09-13 20:17:05 -0400

    Export all components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import Base from './components/Base';
 import Container from './components/Container';
+import Grid from './components/Grid';
+import Button from './components/Button';
+import Image from './components/Image';
+
 import helpers from './helpers';
 
-export { Base, Container, helpers }
+export { Base, Container, Grid, Button, Image, helpers }
