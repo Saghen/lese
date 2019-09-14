3fd4858d6af9d5487e812dc079087b25f63efcd4
Liam Dyer <saghendev@gmail.com>
2019-09-14 11:39:42 -0400

    Cover instead of Fit

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,5 +4,6 @@ import { returnDefault } from "../helpers";
 export default styled.img`
   ${({ responsive }) => responsive && "max-width: 100%;"}
   ${({ center }) => center && "object-position: center;"}
-  ${({ fit }) => fit && `object-fit: ${returnDefault(fit, "string", "cover")}`}
+  ${({ cover }) =>
+    cover && `object-fit: ${returnDefault(fit, "string", "cover")}`}
 `;
