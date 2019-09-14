6c1290a2dd4ee4a8347308fec80fce1229a399bf
Liam Dyer <saghendev@gmail.com>
2019-09-13 20:47:12 -0400

    Add grid test

--- src/test/index.js
+++ src/test/index.js
@@ -11,6 +11,10 @@ render(
       <Button>Hello</Button>
       <Link>Some Link</Link>
     </Container>
+    <Grid columns="400px 400px" yAlign="center" xAlign="center">
+      <Button>WOW</Button>
+      <Image src="https://voicebot.ai/wp-content/uploads/2019/07/www.maxpixel.net-cute-japan-amazon-danbo-cardboard-robot-3781364.jpg" responsive/>
+    </Grid>
   </div>,
   document.querySelector("#root")
 );
