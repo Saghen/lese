77557ef9864507eec838e274e494771842fd4307
Liam Dyer <saghendev@gmail.com>
2019-09-13 20:44:22 -0400

    Add link to test

--- src/test/index.js
+++ src/test/index.js
@@ -3,12 +3,13 @@ import { render } from "react-dom";
 
 import html from './index.html'
 
-import { helpers, Base, Button, Container, Grid, Image } from "../index";
+import { helpers, Base, Button, Container, Grid, Image, Link } from "../index";
 
 render(
   <div>
-    <Container>
+    <Container column xAlign>
       <Button>Hello</Button>
+      <Link>Some Link</Link>
     </Container>
   </div>,
   document.querySelector("#root")
