ffc3d2d0afd19b4fcf9642889b84d5b726bc736f
Liam Dyer <saghendev@gmail.com>
2019-11-22 16:13:22 -0500

    :construction: Child flex alignment

--- src/components/Flex.js
+++ src/components/Flex.js
@@ -1,7 +1,9 @@
+import { css } from "@emotion/core";
 import styled from "@emotion/styled";
 import Base from "./Base";
 
 import { propertyGenerator } from "../helpers";
+import { CLIENT_RENEG_LIMIT } from "tls";
 
 const getFlexProperties = propertyGenerator([
   ["column", () => "flex-direction: column"],
@@ -40,7 +42,38 @@ const getFlexProperties = propertyGenerator([
   ["wrap", { default: "wrap", property: "flex-wrap" }]
 ]);
 
+const getChildFlexProperties = propertyGenerator([
+  [
+    "xAlignSelf",
+    {
+      default: "center",
+      handler: ({ column, xAlignSelf }) =>
+        column ? `align-self: ${xAlignSelf}` : `justify-self: ${xAlignSelf}`
+    }
+  ],
+  [
+    "yAlignSelf",
+    {
+      default: "center",
+      handler: ({ column, yAlignSelf }) =>
+        column ? `justify-self: ${yAlignSelf}` : `align-self: ${yAlignSelf}`
+    }
+  ]
+]);
+
 export default styled(Base)`
   display: flex;
   ${getFlexProperties}
+  ${({ children, column }) => {
+    if (!Array.isArray(children)) return;
+    const properties = [];
+    for (const [i, { props }] of children.entries())
+      if (props.xAlignSelf || props.yAlignSelf)
+        properties.push(`
+        > *:nth-child(${i + 1}) {
+          ${getChildFlexProperties({ ...props, column })}
+        }
+        `);
+    return properties;
+  }}
 `;
