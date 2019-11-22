989bd4abf5dbc949618da116a1fac493a011dd08
Liam Dyer <saghendev@gmail.com>
2019-11-22 00:14:20 -0500

    Fixed exports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import Base from './components/Base';
 import Flex from "./components/Flex";
 import Grid from './components/Grid';
-import Button from './components/Button';
 import Image from './components/Image';
-import Link from './components/Link';
 
 import { propertyGenerator } from './helpers';
 
-export { Base, Flex, Grid, Button, Image, Link, propertyGenerator };
+export { Base, Flex, Grid, Image, propertyGenerator };
