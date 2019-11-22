d640230fe0b6f3d3eef56bdae639f5cb3955d42d
Liam Dyer <saghendev@gmail.com>
2019-11-22 16:13:00 -0500

    :sparkles: Margin/Padding. Remove width and height from props

--- src/components/Base.js
+++ src/components/Base.js
@@ -1,3 +1,4 @@
+import isPropValid from "@emotion/is-prop-valid";
 import styled from "@emotion/styled";
 import { propertyGenerator } from "../helpers";
 
@@ -21,10 +22,15 @@ const getSizeProperties = propertyGenerator([
 ]);
 
 const getLayoutProperties = propertyGenerator([
-  ["relative", () => "position: relative"]
+  ["relative", () => "position: relative"],
+  ["margin", ({ margin }) => `margin: ${margin}`],
+  ["padding", ({ padding }) => `padding: ${padding}`]
 ]);
 
-export default styled.div`
+export default styled("div", {
+  shouldForwardProp: prop =>
+    isPropValid(prop) && ['width', 'height', 'children'].indexOf(prop) !== -1
+})`
   ${getLayoutProperties}
   ${getSizeProperties}
   ${getTextProperties}
