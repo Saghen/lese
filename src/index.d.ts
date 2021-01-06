c069289a54c163399165ea8b6f48479036ceb680
Liam Dyer <liamcyder@gmail.com>
2021-01-06 17:44:57 -0500

    Improving typing

--- src/index.d.ts
+++ src/index.d.ts
@@ -10,30 +10,30 @@ interface BaseProps {
   margin?: string;
   padding?: string;
 }
-declare const Base: React.SFC<BaseProps>;
+declare const Base: React.FC<BaseProps & React.HTMLAttributes<HTMLDivElement>>;
 
 interface FlexChildProps {
   xAlignSelf?: boolean | string;
   yAlignSelf?: boolean | string;
 }
 
-interface FlexProps extends BaseProps {
-  column?: boolean
+interface FlexProps extends BaseProps, React.HTMLAttributes<HTMLDivElement> {
+  column?: boolean;
   xAlign?: boolean | string;
   yAlign?: boolean | string;
   noMarginReset?: boolean;
   separation?: string;
   wrap?: boolean | string;
-  // children?: [React.SFC<FlexChildProps>];
+  // children?: [React.FC<FlexChildProps>];
 }
-declare const Flex: React.SFC<FlexProps>;
+declare const Flex: React.FC<FlexProps>;
 
 interface GridChildProps {
   column?: string;
   row?: string;
 }
 
-interface GridProps extends BaseProps {
+interface GridProps extends BaseProps, React.HTMLAttributes<HTMLDivElement> {
   columns?: string;
   rows?: string;
   autoColumns?: string;
@@ -44,26 +44,36 @@ interface GridProps extends BaseProps {
   xAlign?: boolean | string;
   yAlign?: boolean | string;
   align?: boolean | string;
-  // children?: [React.SFC<GridChildProps>];
+  // children?: [React.FC<GridChildProps>];
 }
-declare const Grid: React.SFC<GridProps>;
+declare const Grid: React.FC<GridProps>;
 
 interface ImageProps {
   responsive?: boolean;
   center?: boolean;
   cover?: boolean;
 }
-declare const Image: React.SFC<ImageProps>;
+declare const Image: React.FC<ImageProps & React.HTMLAttributes<HTMLImageElement>>;
 
 declare type PropertyHandler = (props: object) => string;
 interface PropertyOptions {
   default?: string;
   handler?: PropertyHandler;
-  property?: string
+  property?: string;
 }
 
-declare function propertyGenerator(
-  properties: Array<[string, PropertyOptions | PropertyHandler]>
-);
+declare function propertyGenerator(properties: Array<[string, PropertyOptions | PropertyHandler]>);
 
-export { Base, BaseProps, Flex, FlexProps, FlexChildProps, Grid, GridProps, GridChildProps, Image, ImageProps, propertyGenerator };
+export {
+  Base,
+  BaseProps,
+  Flex,
+  FlexProps,
+  FlexChildProps,
+  Grid,
+  GridProps,
+  GridChildProps,
+  Image,
+  ImageProps,
+  propertyGenerator,
+};
